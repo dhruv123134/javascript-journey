229ec6f6c8567056b26235821666bdcfd43636e5
Mr Dhruv <dhruvchhabhaiya1@gmail.com>
2026-04-28 15:25:03 +0530

    Add functions for addition, even check, and squaring

---
+++ Day-04-Functions/practice.js
@@ -0,0 +1,18 @@
+// Add function
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(5, 3));
+
+// Even check
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+
+// Arrow function
+const square = x => x * x;
+
+console.log(square(4));
