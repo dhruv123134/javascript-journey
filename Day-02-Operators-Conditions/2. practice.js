d696cade188a0b8ce6b7aee029d43fa902b842f2
Mr Dhruv <dhruvchhabhaiya1@gmail.com>
2026-04-28 12:16:00 +0530

    Day 2: Practice problems added

---
+++ Day-02-Operators-Conditions/2. practice.js
@@ -0,0 +1,35 @@
+
+---
+
+# 💻 2. practice.js
+
+```js
+// Even or Odd
+let num = 7;
+
+if (num % 2 === 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
+
+// Positive / Negative / Zero
+let number = -5;
+
+if (number > 0) {
+  console.log("Positive");
+} else if (number < 0) {
+  console.log("Negative");
+} else {
+  console.log("Zero");
+}
+
+// Greater number
+let a = 10;
+let b = 20;
+
+if (a > b) {
+  console.log("A is greater");
+} else {
+  console.log("B is greater");
+}
