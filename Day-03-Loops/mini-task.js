60c8d11218686a0b362e3102eaf771a03711131d
Mr Dhruv <dhruvchhabhaiya1@gmail.com>
2026-04-28 14:50:34 +0530

    Day 3: Mini task added

---
+++ Day-03-Loops/mini-task.js
@@ -0,0 +1,10 @@
+// Student Attendance System
+let totalStudents = 10;
+
+for (let i = 1; i <= totalStudents; i++) {
+  if (i % 2 === 0) {
+    console.log("Student " + i + " is Present");
+  } else {
+    console.log("Student " + i + " is Absent");
+  }
+}
